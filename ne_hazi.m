5f4c9c703d54a14fe4a1a18139e2523346017e7f
BancRoland <78423437+BancRoland@users.noreply.github.com>
2021-12-13 22:19:17 +0100

    grafikonok kiegészítése

--- ne_hazi.m
+++ ne_hazi.m
@@ -14,7 +14,11 @@ Data=Data/max(Data);
 fs=44100;
 % sound(Data,fs);
 code=[1  0  0  1  1  1  0  1  0  1  0  0  0  1  1  1  1  0  1  1  0  1  0  0  0  0  1  0  0  1  0  0];
+figure(1);
+hold on;
+title('Jel Diszkrét Fourier Transzformáltja');
 plot(abs(fft(Data)));
+
 % 867423000/fs=1.9669e+04;
 % 8921/fs=2.0229e+04
 %%
@@ -22,7 +26,6 @@ plot(abs(fft(Data)));
 %%
 x=0:(length(Data)-1);
 %%
-close all;
 % signal=sin(8921*2*pi*x/length(Data));
 
 % signal=exp(1i*8921*2*pi*x/length(Data)); %b13_br.i16
@@ -35,10 +38,12 @@ signal=exp(1i*8930*2*pi*x/length(Data));    %b13_br_att.i16
 mixed=Data.*signal';
 
 lpfMix=lowpass(mixed,10,fs);
-figure(1);
-plot(real(lpfMix));
+figure(2);
 hold on;
+title('Lekevert alulátereszett komplex jel');
+plot(real(lpfMix));
 plot(imag(lpfMix));
+legend('valós','képzetes');
 % figure(2);
 % plot(angle(mixed));
 
@@ -56,8 +61,12 @@ end
 detect=xcorr(incBarker,lpfMix);
 % detect=xcorr(lpfMix,incBarker);
 figure(3)
-plot(real(detect));
+title('Korreláltatott jel komplex jel') 
+plot(real(detect(1:length(detect)/2)));
 hold on;
-plot(imag(detect));
+plot(imag(detect(1:length(detect)/2)));
+legend('valós','képzetes');
 figure(4)
-plot(abs(detect));
+hold on;
+title('Korreláltatott komplex jel abszolút értéke') 
+plot(abs(detect(1:length(detect)/2)));
