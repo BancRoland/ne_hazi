6564b968a1279ac7d5ee2abda14aa91503ef92eb
BancRoland <78423437+BancRoland@users.noreply.github.com>
2021-12-13 23:37:35 +0100

    saját verziók plottolva, befejezve

--- ne_hazi.m
+++ ne_hazi.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 
 %% beolvasás
-filename='b13_br.i16';
+filename='bin_br_att.i16';
 fileID = fopen(filename, 'r', 'ieee-le');
 if fileID == -1, error('Cannot open file: %s', filename); end
 format = 'int16';
@@ -13,26 +13,33 @@ fclose(fileID);
 Data=Data/max(Data);
 fs=44100;
 % sound(Data,fs);
-code=[1  0  0  1  1  1  0  1  0  1  0  0  0  1  1  1  1  0  1  1  0  1  0  0  0  0  1  0  0  1  0  0];
+
 figure(1);
 hold on;
 title('Jel Diszkrét Fourier Transzformáltja');
 plot(abs(fft(Data)));
 
-% 867423000/fs=1.9669e+04;
-% 8921/fs=2.0229e+04
 %%
 % sound(Data,fs);
 %%
-x=0:(length(Data)-1);
-%%
-% signal=sin(8921*2*pi*x/length(Data));
 
-signal=exp(1i*8921*2*pi*x/length(Data)); %b13_br.i16
-% signal=exp(1i*8930*2*pi*x/length(Data));    %b13_br_att.i16
-% plot(signal(1:fs/100));
+%%
+close all;
+
+%DFT-ből származó adat (NEM a legnagyobb komponens, hanem a jel sávközepe!)
+%érdemes lehet kicsit hangolgatni a lekevert eredmény alapján:
+% f=8869; %bin_br.i16 
+f=8860; %bin_br_att.i16 
+
+x=0:(length(Data)-1);
+% signal=sin(f*2*pi*x/length(Data));
+signal=exp(1i*f*2*pi*x/length(Data)); 
+
+%Teszteléshez,sávközép kereéséshez:
+% figure(10);
 % hold on;
-% plot(Data(1:fs/100));
+% plot(real(signal(1:fs/50)));
+% plot(Data(1:fs/50));
 
 % mixed=Data.*signal';
 mixed=Data.*signal';
@@ -47,26 +54,27 @@ legend('valós','képzetes');
 % figure(2);
 % plot(angle(mixed));
 
-%bitsebesség: 1 bit/430 órajel
+
 
 %%
+% Figure(2) alapján a bitsebesség: 1 bit/430 órajel
 incVal=430;
-barker=[1 1 1 1 1 -1 -1 1 1 -1 1 -1 1];
-incBarker=[];
-for inc=1:length(barker)
-    incBarker=[incBarker barker(inc)*ones(1,incVal)];
+code=[1  0  0  1  1  1  0  1  0  1  0  0  0  1  1  1  1  0  1  1  0  1  0  0  0  0  1  0  0  1  0  0];  %saját kódom
+incCode=[]; %incrementált kód (430 szorosra bővített)
+for inc=1:length(code)
+    incCode=[incCode code(inc)*ones(1,incVal)];
 end
 
 %%
-detect=xcorr(incBarker,lpfMix);
-% detect=xcorr(lpfMix,incBarker);
+% detect=xcorr(incCode,lpfMix);
+detect=xcorr(lpfMix,incCode);
 figure(3)
 title('Korreláltatott jel komplex jel') 
-plot(real(detect(1:length(detect)/2)));
+plot(real(detect(length(detect)/2:end)));
 hold on;
-plot(imag(detect(1:length(detect)/2)));
+plot(imag(detect(length(detect)/2:end)));
 legend('valós','képzetes');
 figure(4)
 hold on;
 title('Korreláltatott komplex jel abszolút értéke') 
-plot(abs(detect(1:length(detect)/2)));
+plot(abs(detect(length(detect)/2:end)));
